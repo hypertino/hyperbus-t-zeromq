7218da857970ba78a9a06c6396d4898dd73aaae4
Maga Abdurakhmanov <maqdev@gmail.com>
2017-07-04 19:23:34 +0200

    * updated to latest Hyperbus

--- src/main/scala/com/hypertino/hyperbus/transport/ZMQClient.scala
+++ src/main/scala/com/hypertino/hyperbus/transport/ZMQClient.scala
@@ -54,7 +54,7 @@ class ZMQClient(val serviceResolver: ServiceResolver,
   )
 
   override def ask(message: RequestBase, responseDeserializer: ResponseBaseDeserializer): Task[ResponseBase] = {
-    serviceResolver.lookupService(message.headers.hrl).flatMap { serviceEndpoint ⇒
+    serviceResolver.lookupService(message).flatMap { serviceEndpoint ⇒
       Task.create[ResponseBase] { (_, callback) ⇒
         askThread.ask(message.serializeToString,
           message.headers.correlationId,
